8beeb9acabb82189ca67e2ad9324c17f83a02ad5
Sara Golemon <pollita@php.net>
2015-01-23 03:54:21 -0800

    Allow building statically

---
+++ php_astview.h
@@ -0,0 +1,9 @@
+#ifndef incl_PHP_ASTVIEW_H
+#define incl_PHP_ASTVIEW_H
+
+#include "php.h"
+
+extern zend_module_entry astview_module_entry;
+#define phpext_astview_ptr &astview_module_entry
+
+#endif /* incl_PHP_ASTVIEW_H */
